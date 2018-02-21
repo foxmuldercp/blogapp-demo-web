863ea09c030c1e521b44e069c451f29e65a28c61
Nikolay Turnaviotov <foxmuldercp@users.noreply.github.com>
2018-02-21 05:29:32 +0200

    Drop obsolete code

--- src/containers/Posts.jsx
+++ src/containers/Posts.jsx
@@ -28,12 +28,6 @@ class Posts extends Component {
     this.props.dispatch(postsGet())
   }
 
-  destroyItem(itemId) {
-    this.setState({loading: true})
-    this.props.dispatch(postRemove(itemId))
-    message.info('Delete post called now')
-  }
-
   componentDidMount(){
     if (this.state.items.length == 0) {
       this.getItems()
@@ -45,40 +39,16 @@ class Posts extends Component {
     this.setState(newState)
   }
 
-  shortDate(data) {
-    const date = moment(data)
-    return date.format('DD MMM YY')
-  }
-
-  sortDate(a, b) {
-    const date1 = moment(a)
-    const date2 = moment(b)
-    if(date1.isBefore(date2)) return -1
-    if(date1.isAfter(date2))  return 1
-    return 0
-  }
-
   updateButton() {
     return <Button size='small' onClick={() => {this.getItems()}} loading={this.state.loading} icon='reload'>
       <FormattedMessage id='data.reload' />
     </Button>
   }
 
-
-  actions(record) {
-    let buttons = []
-    if (this.props.user.email) {
-      buttons.push(<Button><Link to={record.url+'/edit'}><FormattedMessage id='common.edit' /></Link></Button>)
-      buttons.push(<Button type='danger' onClick={() => {this.destroyItem(record.id)}}><FormattedMessage id='common.destroy' /></Button>)
-    }
-    return <ButtonGroup>{buttons}</ButtonGroup>
-  }
-
   timeLine(items) {
-    const line = items.map((record) => {
+    return items.map((record) => {
       return <Timeline.Item><PostItem record={record} /></Timeline.Item>
     })
-    return line
   }
 
   render() {
